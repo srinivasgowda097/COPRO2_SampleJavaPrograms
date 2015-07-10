f802068ae2db8dd4b4fcce06a10df15feb641d6c
srinivasgowda097 <srinivasgowda097@gmail.com>
2015-07-10 05:52:57 +0530

    completed #45, #78  resolved #67, #23

--- Cat.java
+++ Cat.java
@@ -5,21 +5,4 @@
 
 public class Cat extends Pet {
 
-	public Cat() {
-		pet_name = "Cat";
-		pet_type = "cat";
-	}
-
-	public String type() {
-		return ( " is a " + pet_type );
-	}
-
-	public void setName( String petName ) {
-		pet_name = petName;
-	}
-
-	public String getName() {
-		return pet_name;
-	}
-}fdnd
-c xc x
+	public Cat() {vnfcv 
